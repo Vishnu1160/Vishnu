7559c46d9d8f27f74c93794e8ece0859b89c1c90
Vishnu1160 <38397167+Vishnu1160@users.noreply.github.com>
2018-04-16 01:37:07 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -4,4 +4,17 @@
 int main()
 {
     int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+ printf("Enter Total Number of Process:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter Burst Time and Priority\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\nP[%d]\n",i+1);
+        printf("Burst Time:");
+        scanf("%d",&bt[i]);
+        printf("Priority:");
+        scanf("%d",&pr[i]);
+        p[i]=i+1;           //contains process number
+    }
  }
