79b87b7c176c11cf255627f5f7834fd51c18376f
Vishnu1160 <38397167+Vishnu1160@users.noreply.github.com>
2018-04-16 01:41:14 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -39,4 +39,17 @@ int main()
         p[i]=p[pos];
         p[pos]=temp;
     }
+ wt[0]=0;    //waiting time for first process is zero
+ 
+    //calculate waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
+
  }
