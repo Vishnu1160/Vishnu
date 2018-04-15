ea32542ef32ae8ab9dd6053d957fc78454b63581
Vishnu1160 <38397167+Vishnu1160@users.noreply.github.com>
2018-04-16 01:55:15 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -2,4 +2,14 @@
 #include<stdio.h>
 int main()
 {
+  int i,j,sum=0,n;
+  int d[20];
+  int disk;
+  int temp,max; 
+  int dloc;
+  clrscr();
+  printf("enter number of location\t");
+  scanf("%d",&n);
+  printf("enter position of head\t");
+  scanf("%d",&disk);
 }
