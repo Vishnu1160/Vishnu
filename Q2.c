b535badd324b25a26a0354b71396ae80e45a64bd
Vishnu1160 <38397167+Vishnu1160@users.noreply.github.com>
2018-04-16 02:01:23 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -12,4 +12,41 @@ int main()
   scanf("%d",&n);
   printf("enter position of head\t");
   scanf("%d",&disk);
+  printf("enter elements of disk queue\n");
+  for(i=0;i<n;i++)
+  {
+  scanf("%d",&d[i]);
+  }
+  d[n]=disk;
+  n=n+1;
+  for(i=0;i<n;i++)
+  {
+  for(j=i;j<n;j++)
+  {
+  if(d[i]>d[j])
+  {
+  temp=d[i];
+  d[i]=d[j];
+  d[j]=temp;
+  }
+  }
+  }
+  max=d[n];
+  for(i=0;i<n;i++)
+  {
+  if(disk==d[i]) { dloc=i; break; }
+  }
+  for(i=dloc;i>=0;i--)
+  {
+  printf("%d -->",d[i]);
+  }
+  printf("0 -->");
+  for(i=dloc+1;i<n;i++)
+  {
+  printf("%d-->",d[i]);
+  }
+  sum=disk+max;
+  printf("\nmovement of total cylinders %d",sum);
+  getch();
+  return 0;
 }
